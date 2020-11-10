9736e5524784ca578418e77eaee543a725fabab0
adnanmc <adnanmcj@gmail.com>
2020-11-09 23:42:06 -0500

    baseline functionality for xl export + firebase deploy

--- src/app/components/json-to-excel/json-to-excel.component.ts
+++ src/app/components/json-to-excel/json-to-excel.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as XLSX from 'xlsx';
+
 @Component({
   selector: 'app-json-to-excel',
   templateUrl: './json-to-excel.component.html',
@@ -8,9 +10,9 @@ export class JsonToExcelComponent implements OnInit {
   constructor() {}
   ngOnInit(): void {}
 
-  jsonText: string = '';
+  jsonText: any;
 
-  fileName: string = 'fileName.xlsx';
+  fileName: string = 'fileName';
 
   editorOptions = {
     theme: 'vs-dark',
@@ -21,10 +23,23 @@ export class JsonToExcelComponent implements OnInit {
     },
   };
 
-  onChange(code) {
-    console.log('new code', code);
+  async paste() {
+    this.jsonText = await navigator.clipboard.readText();
   }
   resetJaonInput() {
-    this.jsonText = '';
+    this.jsonText = null;
+  }
+  downloadXlFile(): void {
+    /* generate worksheet */
+    const ws: XLSX.WorkSheet = XLSX.utils.json_to_sheet(
+      JSON.parse(this.jsonText)
+    );
+
+    /* generate workbook and add the worksheet */
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+
+    /* save to file */
+    XLSX.writeFile(wb, `${this.fileName}.xlsx`);
   }
 }
